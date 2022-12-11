d5af564349dd651d59c02df30b4d22ddc8679b0a
pameirelles <rafaelpmeirelles@gmail.com>
2022-12-11 13:04:57 -0300

    14 finalistas

--- main.py
+++ main.py
@@ -14,32 +14,42 @@ def read_1d_csv(name):
 
 def filter_all():
     df = pd.read_csv("statusinvest-busca-avancada.csv", sep=';', thousands='.', decimal=",")
+    print(df.shape)
+
     df.dropna(inplace=True, subset=['TICKER', 'PRECO', ' LIQUIDEZ MEDIA DIARIA', 'CAGR LUCROS 5 ANOS'])
+    print(df.shape)
+
     df = df[df["TICKER"].str[4] == '3']
+    print(df.shape)
 
     estatais = read_1d_csv('estatais')
     df = df[~df[['TICKER']].isin(estatais).any(axis=1)]
-
-    df = df[df['PRECO'].astype(int) != 0]
-    df = df[df[" LIQUIDEZ MEDIA DIARIA"].astype(float) >= 3000000]
-
+    print(df.shape)
+    df = df[df[" LIQUIDEZ MEDIA DIARIA"].astype(float) >= 700000]
+    print(df.shape)
     graham()
+    print(df.shape)
     gr = pd.read_csv("analise.csv")
     df = df.merge(gr)
     df = df[df["Graham"] < 1]
-
+    print(df.shape)
     df = df[df['CAGR LUCROS 5 ANOS'] >= 30]
+
+    preju = pd.read_csv("prejuizo")
+    df = df.merge(preju)
+    df = df[df["MAX1"] == 1]
+    print(df.shape)
     df.to_csv("filtrado.csv", index=False)
 
 
 def extrai():
-    df = pd.read_csv("filtrado.csv")
+    df = pd.read_csv("statusinvest-busca-avancada.csv", sep=';', thousands='.', decimal=",")
     df = df[["TICKER"]].copy()
     df.to_csv("analise.csv", index=False)
 
 
 def graham():
-    df1 = pd.read_csv("filtrado.csv")
+    df1 = pd.read_csv("statusinvest-busca-avancada.csv", sep=';', thousands='.', decimal=",")
     df2 = pd.read_csv("analise.csv")
     df2["Graham"] = df1["PRECO"] / (df1[" VPA"].astype(float) * df1[" LPA"].astype(float) * 22.5) ** 0.5
     df2.sort_values(by="Graham", inplace=True)
